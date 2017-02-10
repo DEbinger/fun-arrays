edd54adc25ac92751990774dc244d88e451561a2
David <davidebinger888@gmail.com>
2017-02-09 21:59:15 -1000

    initial commit with passing 1st 3 tests, up to sumOfBankBalances

--- array-methods.js
+++ array-methods.js
@@ -1,4 +1,7 @@
+// jshint esversion:6
+
 var dataset = require('./dataset.json');
+var bankBalances = dataset.bankBalances;
 
 /*
   create an array with accounts from bankBalances that are
@@ -7,10 +10,14 @@ var dataset = require('./dataset.json');
 */
 var hundredThousandairs = null;
 
+hundredThousandairs = bankBalances.filter((element, index, array) => {
+  return element.amount > 100000.00;
+});
+
 /*
   set a new key for each object in bankBalances named `rounded`
   the value of this key will be the `amount` rounded to the nearest dollar
-  example 
+  example
     {
       "amount": "134758.44",
       "state": "HI",
@@ -19,11 +26,18 @@ var hundredThousandairs = null;
   assign the resulting array to `roundedDollar`
 */
 var roundedDollar = null;
+roundedDollar = [];
+var newRoundedDollar = bankBalances.map((element, index, array) => {
+  var newObj = {};
+  newObj.rounded = Math.round(element.amount);
+  newObj.state = element.state;
+  roundedDollar.push(newObj);
+});
 
 /*
   set a the `amount` value for each object in bankBalances
   to the value of `amount` rounded to the nearest 10 cents
-  example 
+  example
     {
       "amount": 134758.4,
       "state": "HI"
@@ -31,10 +45,22 @@ var roundedDollar = null;
   assign the resulting array to `roundedDime`
 */
 var roundedDime = null;
+roundedDime = [];
+var newRoundedDime = bankBalances.map((element, index, array) => {
+  var newObj = {};
+  newObj.amount = Math.round(element.amount*10)/10;
+  newObj.state = element.state;
+  roundedDime.push(newObj);
+});
 
 // set sumOfBankBalances to the sum of all amounts in bankBalances
 var sumOfBankBalances = null;
-
+sumOfBankBalances = [];
+var newSumOfBankBalances = bankBalances.reduce((previousValue, currentValue, index, array) => {
+  var newObj = {};
+  sumOfBankBalances.push(newObj);
+  return parseInt(previousValue) + parseInt(currentValue.amount);
+});
 /*
   set sumOfInterests to the sum of the 18.9% interest
   for all amounts in bankBalances
@@ -77,14 +103,14 @@ var stateSums = null;
 
 /*
   set lowerSumStates to an array containing
-  only the two letter state abbreviation of each state 
+  only the two letter state abbreviation of each state
   where the sum of amounts in the state is
     less than 1,000,000
  */
 var lowerSumStates = null;
 
 /*
-  set higherStateSums to be the sum of 
+  set higherStateSums to be the sum of
     all amounts of every state
     where the sum of amounts in the state is
       greater than 1,000,000
@@ -107,7 +133,7 @@ var areStatesInHigherStateSum = null;
 
 /*
   Stretch Goal && Final Boss
-  
+
   set anyStatesInHigherStateSum to be true if
     any of these states have a sum of account values
       greater than 2,550,000
